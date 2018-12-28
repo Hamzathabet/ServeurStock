7fad2189964c06c79d00f3ef1253c619e383dd26
unknown <benthabethamza7@gmail.com>
2018-12-28 10:33:36 +0100

    first serveur commit

---
+++ src/main/java/com/example/demo/ServeurStockApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurStockApplication.class, args);
+	}
+
+}
+
